4ac06fd28ebbc7feb1f840b12e578d6f3f62f694
Salehziad <salehziad1999@gmail.com>
2022-06-30 09:34:28 +0300

    g

--- system/system.js
+++ system/system.js
@@ -3,27 +3,59 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const io = require('socket.io')(PORT);
 const airLineConnection = io.of('/airline');
-io.on('connection', (socket) => {
-    console.log('global connection', socket.id);
-    socket.on('new-flight', (payload) => {
-        console.log('Flight ', payload);
-        airLineConnection.emit('new-flight', payload); // send from global to namespace
-    });
-});
+const uuid=require('uuid').v4;
+const queue ={
+    flights:{
+
+    }
+}
 airLineConnection.on('connection', (socket) => {
-    console.log('airline connection', socket.id);
+    // console.log('global connection', socket.id);
     socket.on('new-flight', (payload) => {
         console.log('Flight ', payload);
-        airLineConnection.emit('new-flight', payload)
+        const id=uuid();
+        queue.flights[id]=payload;
+        let x=queue
+        // console.log({x});
+        // airLineConnection.emit('new-flight', queue.flights); // send from global to namespace
+        airLineConnection.emit('new-flight', {
+            id: id,
+            payload: queue.flights[id]
+        });
     });
+    socket.on('get', () => {
+        // console.log({arr});
+    airLineConnection.emit('xxx', queue.flights); // send from global to namespace
+    })
+
+
+    // console.log('airline connection', socket.id);
+    // socket.on('new-flight', (payload) => {
+    //     // console.log('Flight ', payload);
+    //     airLineConnection.emit('new-flight', payload)
+    // });
     socket.on('took-off', takeOff);
 
     function takeOff(payload) {
         console.log('Flight ', payload);
     }
     socket.on('arrived', arrived);
+
     function arrived(payload) {
         console.log('Flight ', payload);
-        io.emit('arrived', payload); // send from namespace to global
+        io.emit('arrived', payload);
+        // delete queue.flights; // send from namespace to global
+        let x=payload.id
+        // console.log({x});
+        delete queue.flights[payload.id];
+        // console.log({queue});
     }
+    socket.on('got-them',(payload)=>{
+        // console.log(payload.id);
+        Object.keys(queue.flights).forEach((id)=>{
+            
+            delete queue.flights[payload.id];
+        })
+        // console.log('222222222222222222222',{queue});
+    })
 });
